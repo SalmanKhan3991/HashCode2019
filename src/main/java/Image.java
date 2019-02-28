8afb2a0d4ce184a3c17d31d8986f885b39c7fe5c
Salman Khan <salmankhan391991@gmail.com>
2019-02-28 20:27:39 +0000

    Added Image.java
    Image.java

---
+++ src/main/java/Image.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Image {
+    
+    boolean isVertical;
+    List<String> Tags = new ArrayList<String>();
+    int numOfTags, index;
+
+    public Image(boolean isVertical, List<String> T, int Tn, int index) {
+        this.isVertical = isVertical;
+        Tags = T;
+        numOfTags = Tn;
+        this.index = index;
+    }
+
+    public int getNumOfTags(){return numOfTags;}
+
+    public boolean isVertical(){return isVertical;}
+}
