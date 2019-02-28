36d881804f667bf01f06de7fabe9bf0f0a9eb6ac
Nipun Jain <nipunjain24x7@gmail.com>
2019-02-28 20:21:52 +0000

    Create Image.java

---
+++ src/main/java/Image.java
@@ -0,0 +1,18 @@
+import java.util.List;
+public class Image {
+
+    boolean isVertical;
+    List<String> Tags;
+    int numOfTags, index;
+
+    public Image(boolean isVertical, List<String> T, int Tn, int index) {
+        this.isVertical = isVertical;
+        Tags = T;
+        numOfTags = Tn;
+        this.index = index;
+    }
+
+    public int getNumOfTags(){return numOfTags;}
+    public boolean isVertical(){return isVertical;}
+
+}
